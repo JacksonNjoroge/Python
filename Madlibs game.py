224b6131f038e7719b49b8429c049fb95bfac8f1
JacksonNjoroge <jacknjooro254@gmail.com>
2024-12-19 13:22:13 +0300

    Created a mad libs game

---
+++ Madlibs game.py
@@ -0,0 +1,17 @@
+#Madlibs game
+#A word game where you create  a story by filling in the blanks with random words
+
+adjective1 = input("Enter an adjective (description) i.e 'Beautiful, Boring, Exciting':  ")
+noun1 = input("Enter a noun (person, place , thing): ")
+adjective2 = input("Enter an adjective (description) i.e ' Good, big, small': ")
+verb1 = input("Enter a verb ending with 'ing': ")
+adjective3 = input("Enter an adjective (description) i.e 'Happy, sad, angry': ")
+
+print("")
+print("")
+
+#Story template 
+print(f"Today I went to the {adjective1} zoo.")
+print(f"in an exhibit I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
