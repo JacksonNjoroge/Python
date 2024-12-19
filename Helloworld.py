99de4edcf5a489310a90490ca902d501d14fb724
JacksonNjoroge <jacknjooro254@gmail.com>
2024-12-19 11:59:03 +0300

    Hello World and Variables in Python

---
+++ Helloworld.py
@@ -0,0 +1,4 @@
+#This is my first python program
+print("Hello world")
+print("I love Pizza")
+
---
+++ Variables.py
@@ -0,0 +1,36 @@
+#String
+first_name = "Jack"
+email = "jack@example.com"
+food = "Pizza"
+
+print(f"Hello {first_name}")
+print(f"Would you like {food}")
+print(f"your email is {email}")
+print(f"My name is {first_name} and my email is {email} and i like to eat {food}")
+
+#Integer
+
+age = 25
+quantity = 3
+num_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You're buying {quantity} items")
+print(f"Your class has {num_of_students} students")
+
+#Float
+price = 10.99
+gpa = 4.2
+distance = 5.5 
+
+print(f"The price is ${price}")
+print(f"Your GPA is {gpa}")
+print(f"You ran {distance} kilometers")
+
+#Boolean
+
+is_Student = False
+if is_Student:
+    print("You are a student")
+else:
+    print("You are NOT a student")
