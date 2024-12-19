0cabc5a897db6731361160ea9b881bf3ded9d4d3
JacksonNjoroge <jacknjooro254@gmail.com>
2024-12-19 12:50:04 +0300

    Simple program that accepts user input and calculates the area of a rectangle

---
+++ Area.py
@@ -0,0 +1,15 @@
+#Area of a Rectangle
+length = float(input("Enter the Length of the rectangle: "))
+Width = float(input("Enter the Width of the rectangle: "))
+
+area = length * Width
+
+print(f"The area of the rectangle is: {area}cm²")
+
+print("")
+print("")
+#Area of a circle
+radius = int(input("Enter the raidus of the circle: "))
+circle_area = 3.14 * radius * radius
+
+print(f"The area of the circle is {circle_area}cm²")
