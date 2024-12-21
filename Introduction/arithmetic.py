4abce7ba90d71d23f894608275ad84da7c2d9d44
JacksonNjoroge <jacknjooro254@gmail.com>
2024-12-21 18:12:58 +0300

    Arithmetic ops

---
+++ Introduction/arithmetic.py
@@ -0,0 +1,11 @@
+import math 
+#radius = float(input("Enter the raidus of the circle: "))
+#circumfrence = 2 * math.pi * radius
+#area = math.pi * pow(radius, 2)
+#print(f"The Circumfrence of the circle is: {round(circumfrence, 2)}cm #while the Area of the circle is: {round(area, 2)}cm²")
+
+#Hypoteneuse of a traingle
+height = float(input("Enter the height of the triangle: "))
+base = float(input("Enter the base of the triangle: "))
+hypoteneuse = math.sqrt(pow(height, 2) + pow(base, 2))
+print(f"The length of the hypotenuse of the triangle is: {hypoteneuse}")
